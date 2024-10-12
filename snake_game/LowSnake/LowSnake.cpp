f759f92f99f6122648032981011aeb0fd18ee099
[BOT] Nosense <uchihanosense@gmail.com>
2024-10-12 13:36:02 +0700

    daily updates

--- snake_game/LowSnake/LowSnake.cpp
+++ snake_game/LowSnake/LowSnake.cpp
@@ -3,64 +3,92 @@
 #include <iostream>
 #include "SnakeLib.h"
 
-int snake_length = 4;
+int g_nSnakeLength = 4,
+    g_nPosDefault[2] = { 50, 12 },
+    g_nBorderGame[4] = { 5, 3, 105, 23 }    // A(5,3), console screen (100x20)
+    ;
 
-void draw(char ch, int x, int y)
+int CheckLimited(int n_PosX, int n_PosY, int check)
 {
-    gotoXY(x, y);
-    std::cout << ch;
+    return  (n_PosY == (g_nBorderGame[3] - 1)) ? 1 :
+            (n_PosY == (g_nBorderGame[0] - 1)) ? 0 :
+            (n_PosX == (g_nBorderGame[0] + 1)) ? 2 :
+            (n_PosX == (g_nBorderGame[2] - 3)) ? 3 :
+            check;
 }
 
-void snake_draw(int x[], int y[])
+void Draw(char cCharacter, int nPosX, int nPosY)
 {
-    if (snake_length == 4)
+    gotoXY(nPosX, nPosY);
+    std::cout << cCharacter;
+}
+
+void SnakeDraw(int nPosX[], int nPosY[])
+{
+    if (g_nSnakeLength == 4)
     {
-        draw('O', x[0], y[0]);
-        draw('0', x[1], y[1]);
-        draw('o', x[2], y[2]);
-        draw('.', x[3], y[3]);
+        Draw('O', nPosX[0], nPosY[0]);
+        Draw('0', nPosX[1], nPosY[1]);
+        Draw('o', nPosX[2], nPosY[2]);
+        Draw('.', nPosX[3], nPosY[3]);
     }
     else 
     {
-        draw('O', x[0], y[0]);
-        draw('0', x[1], y[1]);
-        draw('.', x[snake_length], y[snake_length]);
-        for (int i = 2; i < snake_length; i++)
-            draw('o', x[i], y[i]);
+        Draw('O', nPosX[0], nPosY[0]);
+        Draw('0', nPosX[1], nPosY[1]);
+        Draw('.', nPosX[g_nSnakeLength], nPosY[g_nSnakeLength]);
+        for (int i = 2; i < g_nSnakeLength; i++)
+            Draw('o', nPosX[i], nPosY[i]);
     }
 }
 
-void snake_sketch(int x[], int y[])
+void SnakeSketch(int nPosX[], int nPosY[])
 {
-    int i = 0, a = 50, b = 12;
-    for (int i = 0; i < snake_length; i++)
+    for (int i = 0; i < g_nSnakeLength; i++)
     {
-        x[i] = a;
-        y[i] = b;
-        a--;
-        std::cout << a;
+        nPosX[i] = g_nPosDefault[0];
+        nPosY[i] = g_nPosDefault[1];
+        g_nPosDefault[0]--;
+        std::cout << g_nPosDefault[0];
     }
 }
 
-void add_first(int arr[], int arr_length, int new_value)
+void InsertBegin(int nArr[], int nLength, int nValue)
 {
-    std::cout << arr_length;
-    for (int i = arr_length; i > 0; i--)
-    {
-        std::cout << i + ", " + arr[i];
-        arr[i] = arr[i - 1];
-    }
-    arr[0] = new_value;
-    arr_length++;
+    for (int i = nLength; i > 0; i--)
+        nArr[i] = nArr[i - 1];
+    nArr[0] = nValue;
+    nLength++;
 }
 
-void remove(int arr[], int index)
+void RemoveLast(int nArr[], int nIndex)
 {
-    for (int i = index; i < snake_length; i++)
+    for (int i = nIndex; i < g_nSnakeLength; i++)
+        nArr[i] = nArr[i + 1];
+    g_nSnakeLength--;
+}
+
+void BorderDraw()
+{
+    int a = g_nBorderGame[0],
+        b = g_nBorderGame[0],
+        e = g_nBorderGame[1],
+        f = g_nBorderGame[1];
+
+    while (a <= g_nBorderGame[2])
     {
-        arr[i] = arr[i + 1];
+        Draw('+', a, g_nBorderGame[1]);
+        a += 1;
+        Draw('+', b, g_nBorderGame[3]);
+        b += 1;
     }
-    snake_length--;
+    while (e <= g_nBorderGame[3])
+    {
+        Draw('+', g_nBorderGame[0], e);
+        e += 1;
+        Draw('+', g_nBorderGame[2], f);
+        f += 1;
+    }
 }
 
 void snake_move(int x[], int y[], int a, int b, int length)
@@ -68,68 +96,48 @@ void snake_move(int x[], int y[], int a, int b, int length)
     //add_array(x, length, a );
 }
 
-void border_draw()
-{
-    int a = 10, b = 1, c = 100, d = 26, i = a, j = a, k = b, l = b;
-    
-    while (i < c)
-    {
-        draw('+', i, b);
-        i += 1;
-        draw('+', j, d);
-        j += 1;
-    }
-    while (k <= d)
-    {
-        draw('+', a, k);
-        k += 1;
-        draw('+', c, l);
-        l += 1;
-    }
-}
-
 int main()
 {
-    int check = 0;  // 0: cham bien tren -> di xuong
+    int move = 0;  // 0: cham bien tren -> di xuong
                     // 1: cham bien duoi -> di len
                     // 2: cham bien trai -> sang phai
                     // 3: cham bien phai -> sang trai
     int color = 1;
-    int x = 10, y = 0;
-    int x_old = x, y_old = y;
+    int x = g_nPosDefault[0], y = g_nPosDefault[1];
     int toadoX[100], toadoY[100];
 
+    BorderDraw();
 
-    //border_draw();
-    /*snake_sketch(toadoX, toadoY, snake_length);
-    snake_draw(toadoX, toadoY, snake_length);*/
+    /*SnakeSketch(toadoX, toadoY, g_nSnakeLength);
+    SnakeDraw(toadoX, toadoY, g_nSnakeLength);*/
     
-    //snake_move(toadoX, toadoY, x, y, snake_length);
+    //snake_move(toadoX, toadoY, x, y, g_SnakeLength);
 
-    /*for (int i = snake_length; i > 0; i--)
+    /*for (int i = g_nSnakeLength; i > 0; i--)
     {
         std::cout << i + ", " + toadoX[i];
         toadoX[i] = toadoX[i - 1];
     }
     toadoX[0] = 0;*/
-    //snake_length++;
+    //g_SnakeLength++;
 
-    for (int i = 4; i > 0; i--)
+    /*for (int i = 4; i > 0; i--)
     {
         std::cout << i ;
         toadoX[i] = toadoX[i - 1];
         std::cout << toadoX[i];
         std::cout << '\n';
-    }
+    }*/
 
-    /*while (true)
+    while (true)
     {
-        gotoXY(x_old, y_old);
-        std::cout << "    ";
         gotoXY(x, y);
-        std::cout << "hehe";
-        x_old = x;
-        y_old = y;
+        std::cout << "   ";
+        gotoXY(g_nPosDefault[0], g_nPosDefault[1]);
+        std::cout << "[!]";
+
+        x = g_nPosDefault[0];
+        y = g_nPosDefault[1];
 
         SetColor(color);
         color++;
@@ -141,19 +149,18 @@ int main()
             if (ch == -32)
             {
                 ch = _getch();
-                ch = (ch == 72) ? check = 1 : (ch == 80) ? check = 0 : (ch == 75) ? check = 3 : (ch == 77) ? check = 2 : ch;
+                ch = (ch == 72) ? move = 1 : (ch == 80) ? move = 0 : (ch == 75) ? move = 3 : (ch == 77) ? move = 2 : ch;
             }
-            ch = (ch == 's') ? check = 1 : (ch == 'x') ? check = 0 : (ch == 'z') ? check = 3 : (ch == 'c') ? check = 2 : ch;
+            ch = (ch == 's') ? move = 1 : (ch == 'x') ? move = 0 : (ch == 'z') ? move = 3 : (ch == 'c') ? move = 2 : ch;
         }
 
-        y = (check == 0) ? (y + 1) : (check == 1) ? (y - 1) : y;
-        x = (check == 2) ? (x + 1) : (check == 3) ? (x - 1) : x;
-        check = (y == 20) ? 1 : (y == 0) ? 0 : (x == 0) ? 2 : (x == 100) ? 3 : check;
+        g_nPosDefault[1] = (move == 0) ? (g_nPosDefault[1] + 1) : (move == 1) ? (g_nPosDefault[1] - 1) : g_nPosDefault[1];
+        g_nPosDefault[0] = (move == 2) ? (g_nPosDefault[0] + 1) : (move == 3) ? (g_nPosDefault[0] - 1) : g_nPosDefault[0];
+        move = CheckLimited(g_nPosDefault[0], g_nPosDefault[1], move);
 
         Sleep(100);
-    }*/
+    }
 
-    
     _getch();
     return 0;
 }
