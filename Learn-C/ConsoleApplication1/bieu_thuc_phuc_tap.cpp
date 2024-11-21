2912d43c3de1c07aa7b90189ee9b98bee6ed3512
[BOT] Nosense <uchihanosense@gmail.com>
2024-11-21 12:07:00 +0700

    bieu_thuc_phuc_tap

---
+++ Learn-C/ConsoleApplication1/bieu_thuc_phuc_tap.cpp
@@ -0,0 +1,116 @@
+﻿#include <iostream>
+#include <string>
+#include <stack>
+#include <sstream>
+#include <cctype>
+#include <algorithm>
+using namespace std;
+
+/*
+    input: x * 3
+    output: xxx -> cộng chuỗi thông thường
+
+    input: ( ( (x*31) - x ) / x )
+    output: 30 -> các giá trị x được triệt tiêu
+*/
+
+
+// Hàm kiểm tra biểu thức có dạng "x * số"
+bool isStringConcatenation(const string& expr) {
+    size_t pos = expr.find('*');
+    if (pos != string::npos) {
+        string left = expr.substr(0, pos);
+        string right = expr.substr(pos + 1);
+        // Kiểm tra bên trái là 'x' và bên phải là số
+        return left.find('x') != string::npos && all_of(right.begin(), right.end(), ::isdigit);
+    }
+    return false;
+}
+
+// Hàm xử lý cộng chuỗi
+string handleStringConcatenation(const string& expr) {
+    size_t pos = expr.find('*');
+    int count = stoi(expr.substr(pos + 1)); // Lấy số sau dấu '*'
+    return string(count, 'x'); // Lặp 'x' count lần
+}
+
+// Hàm tính giá trị biểu thức toán học
+int evaluateExpression(const string& expr, int x) {
+    stack<int> values;
+    stack<char> ops;
+
+    for (int i = 0; i < expr.length(); i++) {
+        if (expr[i] == ' ') continue;
+
+        if (expr[i] == '(') {
+            ops.push('(');
+        }
+        else if (expr[i] == ')') {
+            while (ops.top() != '(') {
+                int val2 = values.top(); values.pop();
+                int val1 = values.top(); values.pop();
+                char op = ops.top(); ops.pop();
+
+                int result = 0;
+                if (op == '*') result = val1 * val2;
+                else if (op == '-') result = val1 - val2;
+                else if (op == '/') result = val1 / val2;
+
+                values.push(result);
+            }
+            ops.pop(); // Loại bỏ dấu '('
+        }
+        else if (expr[i] == '*' || expr[i] == '-' || expr[i] == '/') {
+            ops.push(expr[i]);
+        }
+        else if (isdigit(expr[i]) || expr[i] == 'x') {
+            int val = 0;
+            if (expr[i] == 'x') {
+                val = x;
+            }
+            else {
+                while (i < expr.length() && isdigit(expr[i])) {
+                    val = val * 10 + (expr[i] - '0');
+                    i++;
+                }
+                i--;
+            }
+            values.push(val);
+        }
+    }
+
+    while (!ops.empty()) {
+        int val2 = values.top(); values.pop();
+        int val1 = values.top(); values.pop();
+        char op = ops.top(); ops.pop();
+
+        int result = 0;
+        if (op == '*') result = val1 * val2;
+        else if (op == '-') result = val1 - val2;
+        else if (op == '/') result = val1 / val2;
+
+        values.push(result);
+    }
+
+    return values.top();
+}
+
+int main() {
+    string input;
+    cout << "Nhập biểu thức: ";
+    getline(cin, input);
+
+    if (isStringConcatenation(input)) {
+        // Xử lý trường hợp cộng chuỗi
+        string result = handleStringConcatenation(input);
+        cout << "Kết quả: " << result << endl;
+    }
+    else {
+        // Xử lý trường hợp biểu thức toán học
+        int x = 1; // Giả sử x = 1
+        int result = evaluateExpression(input, x);
+        cout << "Kết quả: " << result << endl;
+    }
+
+    return 0;
+}
--- Learn-C/ConsoleApplication1/tham_tri_tham_chieu.cpp
+++ Learn-C/ConsoleApplication1/tham_tri_tham_chieu.cpp
@@ -1,65 +1,75 @@
-﻿#include <iostream>
-#include <string> 
-#include <typeinfo>
-
-using namespace std;
-
-/* Đổi chỗ 2 giá trị */
-
-void swapByValue(int x, int y)
-{
-	int temp = x;
-	x = y;
-	y = temp;
-}
-
-void swapByReference(int& x, int& y)
-{
-	int temp = x;
-	x = y;
-	y = temp;
-}
-
-
-/* tính tổng hiệu 2 số */
-struct ThamTri
-{
-	int sum;
-	int diff;
-};
-
-ThamTri caculator(int x, int y)
-{
-	ThamTri result;
-	result.sum = x + y;
-	result.diff = x - y;
-	return result; // tất cả các giá trị đều gói gọn vào result -> dễ quản lý với nhiều tham số
-}
-
-void caculatorByReferenceValue(int x, int y, int& sum, int& diff)
-{
-	sum = x + y;
-	diff = x - y;
-}
-
-void TongHieu(int a, int b, int sum, int diff)
-{
-	ThamTri result = caculator(a, b); // clone ra 1 result để hứng kết quả của hàm caculator() bên trên -> x2 bộ nhớ 
-	cout << result.sum << "end " << result.diff << endl;
-
-	caculatorByReferenceValue(a, b, sum, diff); // cập nhật trực tiếp vào 2 biến đc tạo trong main 
-	cout << sum << "end " << diff << endl;
-}
-
-
-
-
-int main()
-{
-	int a = 10, b = 3;
-	int sum, diff;
-
-	//TongHieu(a, b, sum, diff);
-
-	return 0;
-}
+﻿//#include <iostream>
+//#include <string> 
+//#include <typeinfo>
+//
+//using namespace std;
+//
+///* Đổi chỗ 2 giá trị */
+//
+//void swapByValue(int x, int y)
+//{
+//	int temp = x;
+//	x = y;
+//	y = temp;
+//}
+//
+//void swapByReference(int& x, int& y)
+//{
+//	int temp = x;
+//	x = y;
+//	y = temp;
+//}
+//
+//
+///* tính tổng hiệu 2 số */
+//struct ThamTri
+//{
+//	int sum;
+//	int diff;
+//};
+//
+//ThamTri caculator(int x, int y)
+//{
+//	ThamTri result;
+//	result.sum = x + y;
+//	result.diff = x - y;
+//	return result; // tất cả các giá trị đều gói gọn vào result -> dễ quản lý với nhiều tham số
+//}
+//
+//void caculatorByReferenceValue(int x, int y, int& sum, int& diff)
+//{
+//	sum = x + y;
+//	diff = x - y;
+//}
+//
+//void TongHieu(int a, int b, int sum, int diff)
+//{
+//	ThamTri result = caculator(a, b); // clone ra 1 result để hứng kết quả của hàm caculator() bên trên -> x2 bộ nhớ 
+//	cout << result.sum << "end " << result.diff << endl;
+//
+//	caculatorByReferenceValue(a, b, sum, diff); // cập nhật trực tiếp vào 2 biến đc tạo trong main 
+//	cout << sum << "end " << diff << endl;
+//}
+//
+//
+//
+///* đảo ngược mảng*/
+//void reverseArrayByValue(int arr[], int size)
+//{
+//	int temp = arr[size];
+//	for (int i = size; i > 0; i--)
+//	{
+//		arr[size] = a[]
+//	}
+//}
+//
+//
+//int main()
+//{
+//	int a = 10, b = 3;
+//	int sum, diff;
+//
+//	//TongHieu(a, b, sum, diff);
+//
+//	return 0;
+//}
