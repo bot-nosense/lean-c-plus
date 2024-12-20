b217ad696ccc1b52d2088beb8723bf388f8bc84d
14AlC6-nosense <uchihanosense@gmail.com>
2024-12-20 17:42:05 +0700

    daily

--- vsc-learn-c/toan-tu-logic.cpp
+++ vsc-learn-c/toan-tu-logic.cpp
@@ -3,11 +3,11 @@
 int main()
 {
     int a = 0;
-    std::cout << "nhap vao 1 so" << std::endl;
-    std::cin >> a;
+    std::cout << "enter a number" << std::endl;
+    std::cin >> a; 
 
-    if (a > 0 && a < 10) std::cout << "a nam trong khoan tu 0 toi 10";
-    else std::cout << "a k nam trong khoang 0-10";
+    if (a > 0 && a < 10) std::cout << "number is in the range 0 - 10";
+    else std::cout << "number is not in the range 0 - 10";
 
     std::cin.ignore();
     std::cin.get();
